fe763cdf82d390cb72d7f5ded92007a41c1cd52c
Dominic Fernandes <dominic.fernandes@panion.org>
2022-12-17 11:07:29 +0100

    initial dapp setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getProvider, getWeb3 } from './web3';
+import Web3 from 'web3';
+import ERC20ABI from './abi/ERC20.json';
+import { tokenAddress } from './tokenConfigs';
+
+const connectWallet = async () => {
+  const web3 =  getWeb3();
+  await web3.eth.requestAccounts();
+  registerEvents();
+}
+
+const registerEvents = () => {
+  const provider = getProvider();
+
+  provider.on("accountsChanged", (accounts: string[]) => {
+    console.log(accounts)
+  });
+
+  provider.on('chainChanged', (chainId: number) => {
+    console.log(Web3.utils.toDecimal(chainId))
+  });
+};
+
+const getTokenName = async() => {
+  const web3 = getWeb3();
+  const contract = new web3.eth.Contract(ERC20ABI as any, tokenAddress.ethereum.chainLink);
+  const result = await contract.methods.approve("0x4a5c8a9FC9b2Da27E46057584F3E08A1aC90F958", Web3.utils.toWei("10")).send({
+    from: "0x2551e9C45860ca011C8f8df6B62F0f4d7473a3e9",
+  });
+  console.log(result);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={connectWallet}>Connect</button>
+        <button onClick={getTokenName}>Get Name</button>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/tokenConfigs.ts
@@ -0,0 +1,5 @@
+export const tokenAddress = {
+    ethereum: {
+        chainLink: "0x7af963cf6d228e564e2a0aa0ddbf06210b38615d"
+    }
+};
